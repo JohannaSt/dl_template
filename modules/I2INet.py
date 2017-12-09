c7674b987461ac6d7845cfbf345a4ad29895b64c
gabriel <gmaher2@hotmail.com>
2017-12-09 13:00:22 -0800

    model template

---
+++ modules/I2INet.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+"""
+This file builds the I2INet network and sets up the required
+
+*file reader
+*file post processor
+*trainer
+*saver
+*predictor
+*logger
+
+The idea is that by keeping these definitions in a separate file
+we can make the choice of network configurable by simply referencing
+a particular file
+"""
+
+def read_file(filename):
+
+def normalize(Tuple):
+    
+def augment(Tuple):
+    
+def process_tuple(Tuple):
+    
+def train_step(Tuple):
+
+def save_model():
+    
+def predict(Tuple):
+    
+def log(Tuple):
